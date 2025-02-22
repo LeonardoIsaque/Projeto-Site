fca069656d634bb07a25df5e30fce9d857da07f2
Leonardokask <106610108+Leonardokask@users.noreply.github.com>
2025-02-22 00:10:58 -0300

    Add projeto Spotify

---
+++ Projeto-Spotify/components/Main.jsx
@@ -0,0 +1,213 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <div className="item-list" >
+              <div className='item-list__header'>
+              
+              <h2>Artistas Populares</h2>
+              <a className='item-list__link' href='/' >Mostrar Tudo</a>
+              </div>
+
+              <div className='item-list__container'>
+
+                <div className="single-item">
+                  <div className='single-item__div-image-button'>
+                        <div className='single-item__div-image'>
+                    
+
+                        <img className="single-item__image" 
+                          src="https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706" 
+                          alt="Imagem Artista X" />
+                        </div>
+
+                  <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                  </div>         
+                  
+                  <div className='single-item__texts'>
+                    <div className='single-item__2lines'>
+
+                      <p className='single-item__title'>Jorge & Mateus</p>
+                      <p className='single-item__type'>Artista</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="single-item">
+                  <div className='single-item__div-image-button'>
+                        <div className='single-item__div-image'>
+                    
+
+                        <img className="single-item__image" 
+                          src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" 
+                          alt="Imagem Artista X" />
+                        </div>
+
+                  <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                  </div>         
+                  
+                  <div className='single-item__texts'>
+                    <div className='single-item__2lines'>
+
+                      <p className='single-item__title'>Henrique & Juliano</p>
+                      <p className='single-item__type'>Artista</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="single-item">
+                  <div className='single-item__div-image-button'>
+                        <div className='single-item__div-image'>
+                    
+
+                        <img className="single-item__image" 
+                          src="https://i.scdn.co/image/ab67616100005174b4c07f06044ca6f1a8548192" 
+                          alt="Imagem Artista X" />
+                        </div>
+
+                  <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                  </div>         
+                  
+                  <div className='single-item__texts'>
+                    <div className='single-item__2lines'>
+
+                      <p className='single-item__title'>Nattan</p>
+                      <p className='single-item__type'>Artista</p>
+                    </div>
+                  </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div className="item-list" >
+              <div className='item-list__header'>
+              
+              <h2>Músicas Populares</h2>
+              <a className='item-list__link' href='/' >Mostrar Tudo</a>
+              </div>
+
+              <div className='item-list__container'>
+
+
+                <div className="single-item">
+                  <div className='single-item__div-image-button'>
+                        <div className='single-item__div-image'>
+                    
+
+                        <img className="single-item__image" 
+                          src="https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706" 
+                          alt="Imagem Artista X" />
+                        </div>
+
+                  <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                  </div>         
+                  
+                  <div className='single-item__texts'>
+                    <div className='single-item__2lines'>
+
+                      <p className='single-item__title'>Jorge & Mateus</p>
+                      <p className='single-item__type'>Artista</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="single-item">
+                  <div className='single-item__div-image-button'>
+                        <div className='single-item__div-image'>
+                    
+
+                        <img className="single-item__image" 
+                          src="https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706" 
+                          alt="Imagem Artista X" />
+                        </div>
+
+                  <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                  </div>         
+                  
+                  <div className='single-item__texts'>
+                    <div className='single-item__2lines'>
+
+                      <p className='single-item__title'>Jorge & Mateus</p>
+                      <p className='single-item__type'>Artista</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="single-item">
+                  <div className='single-item__div-image-button'>
+                        <div className='single-item__div-image'>
+                    
+
+                        <img className="single-item__image" 
+                          src="https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706" 
+                          alt="Imagem Artista X" />
+                        </div>
+
+                  <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                  </div>         
+                  
+                  <div className='single-item__texts'>
+                    <div className='single-item__2lines'>
+
+                      <p className='single-item__title'>Jorge & Mateus</p>
+                      <p className='single-item__type'>Artista</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="single-item">
+                  <div className='single-item__div-image-button'>
+                        <div className='single-item__div-image'>
+                    
+
+                        <img className="single-item__image" 
+                          src="https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706" 
+                          alt="Imagem Artista X" />
+                        </div>
+
+                  <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                  </div>         
+                  
+                  <div className='single-item__texts'>
+                    <div className='single-item__2lines'>
+
+                      <p className='single-item__title'>Jorge & Mateus</p>
+                      <p className='single-item__type'>Artista</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="single-item">
+                  <div className='single-item__div-image-button'>
+                        <div className='single-item__div-image'>
+                    
+
+                        <img className="single-item__image" 
+                          src="https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706" 
+                          alt="Imagem Artista X" />
+                        </div>
+
+                  <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+                  </div>         
+                  
+                  <div className='single-item__texts'>
+                    <div className='single-item__2lines'>
+
+                      <p className='single-item__title'>Jorge & Mateus</p>
+                      <p className='single-item__type'>Artista</p>
+                    </div>
+                  </div>
+                </div>
+                
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
